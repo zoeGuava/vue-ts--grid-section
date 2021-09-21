bdbd0d3eccbeff27c117665fbb941ce87e627ecc
Kraemer <kraemer.tsai@tpisoftware.com>
2021-09-21 23:48:09 +0800

    public test

--- src/components/card-data.ts
+++ src/components/card-data.ts
@@ -1,5 +1,5 @@
 import { CardSpec } from './card-spec';
-import Daniel from '../../public/images/image-daniel.jpg';
+// import Daniel from '../../public/images/image-daniel.jpg';
 import Jonathan from '../../public/images/image-jonathan.jpg';
 import Jeanette from '../../public/images/image-jeanette.jpg';
 import Patrick from '../../public/images/image-patrick.jpg';
@@ -10,7 +10,7 @@ export const CARD_DATA:CardSpec[] = [
     header: {
       name: 'Daniel Clifford',
       status: 'Verified Graduate',
-      img: Daniel
+      img: '../../public/images/image-daniel.jpg'
     },
     slogan: `I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny’s worth.`,
     content: `I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup.`
